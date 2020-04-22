6d69d244ab74b054b0d3ea05c15e13c873026eee
Alex Brown <abrown252@gmail.com>
2020-04-22 14:36:08 +0100

    Testing key commands

---
+++ Sources/SwiftPasscode/ViewControllers/PasscodeViewController/PasscodeViewController+UIKeyCommand.swift
@@ -0,0 +1,34 @@
+//
+//  PasscodeViewController+UIKeyCommand.swift
+//  SwiftPasscode
+//
+//  Created by Alex Brown on 22/04/2020.
+//
+
+import Foundation
+import UIKit
+
+extension PasscodeViewController {
+    
+    public override var canBecomeFirstResponder: Bool {
+        return true
+    }
+    
+    public override var keyCommands: [UIKeyCommand]? {
+        var commands = [UIKeyCommand]()
+        for i in 0...9 {
+            commands.append(UIKeyCommand(input: "\(i)", modifierFlags: [], action: #selector(valueSelected(command:)), discoverabilityTitle: "\(i)"))
+        }
+        
+        return commands
+    }
+    
+    @objc func valueSelected(command: UIKeyCommand) {
+        guard
+            let input = command.input,
+            let value = Int(input)
+            else {return}
+        
+        print(value)
+    }
+}
